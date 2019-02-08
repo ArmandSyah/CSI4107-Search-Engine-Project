ea15ac4fbc05eb1a4eab1c87d7e0bfe1f09f09bf
Armand Syahtama <armand.syahtama@gmail.com>
2019-02-08 11:56:18 -0500

    vector scoring

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from pprint import pprint
+from utilities import *
 
 
 class Dictionary():
@@ -16,35 +17,20 @@ class Dictionary():
             'stemmed': set(),
             'normalized': set()
         }
-        self.stop_words = set(stopwords.words('english')) | set(
-            stopwords.words('french'))
-        self.stemmer = PorterStemmer()
 
     def make_dictionary(self):
         with open('corpus.json') as corpus:
             data = json.load(corpus)
 
             for entry in data:
-                tokenized_fulltext = [word.lower()
+                tokenized_fulltext = [lowercase_folding(word)
                                       for word in word_tokenize(entry['fulltext'])]
 
                 self.dict['unaltered'] |= set(tokenized_fulltext)
 
                 self.dict['altered'] |= normalize(stem(remove_stopwords(
-                    tokenized_fulltext, self.stop_words), self.stemmer))
+                    tokenized_fulltext)))
 
         with open('dictionary.json', 'w') as outfile:
             uo_dict_lists = {k: list(v) for (k, v) in self.dict.items()}
             json.dump(uo_dict_lists, outfile, ensure_ascii=False, indent=4)
-
-
-def normalize(text):
-    return {word.translate(str.maketrans('', '', string.punctuation)).lower() for word in text}
-
-
-def remove_stopwords(text, stop_words):
-    return set([w.lower() for w in text if not w in stop_words])
-
-
-def stem(text, stemmer):
-    return set([stemmer.stem(w).lower() for w in text])
--- vectorspacemodel/VectorSpaceModel.py
+++ vectorspacemodel/VectorSpaceModel.py
@@ -1,8 +1,13 @@
 import json
 import math
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from collections import defaultdict
+from utilities import *
 
 # tf uses raw frequencies
+# assume all weights in each word of query have weight 1
 
 
 class VectorSpaceModel():
@@ -16,6 +21,14 @@ class VectorSpaceModel():
             inv_index,
             compute_idf(self.complete_set, inv_index))
 
+    def retrieve(self, query):
+        query = lowercase_folding(query)
+        tokens = remove_stopwords(word_tokenize(query))
+        query_vector = (1) * len(tokens)
+        doc_vectors = compute_doc_vectors(
+            self.complete_set, self.tf_idf_matrix, tokens)
+        scores = compute_vector_scores(query_vector, doc_vectors)
+
 
 def compute_idf(complete_set, inverted_index):
     number_of_docs = len(complete_set)
@@ -24,7 +37,7 @@ def compute_idf(complete_set, inverted_index):
 
 
 def compute_tf_idf(complete_set, inverted_index, idf_index):
-    tf_idf = {}
+    tf_idf = defaultdict(lambda: defaultdict(int))
     for word, docs in inverted_index.items():
         placeholder = defaultdict(int)
         for appearance in docs:
@@ -32,3 +45,18 @@ def compute_tf_idf(complete_set, inverted_index, idf_index):
                 idf_index[word]
         tf_idf[word] = placeholder
     return tf_idf
+
+
+def compute_doc_vectors(complete_set, tf_idf_matrix, tokens):
+    doc_vectors = defaultdict(tuple)
+    for doc_id in complete_set:
+        vector = []
+        for token in tokens:
+            set_of_docweights = tf_idf_matrix[token]
+            weight = set_of_docweights[doc_id]
+            vector.append(weight)
+        doc_vectors[doc_id] = vector
+    return doc_vectors
+
+
+def compute_vector_scores
